0a227d9b31435c17c4ecf7bab99cd22b89f52b6d
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-03-17 15:16:36 +0200

    Return ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         path: path.join(__dirname, 'dist/'),
-        filename: `index.js`,
+        filename: 'index.js',
     },
     module: {
         rules: [
